96538489a6abe3c132e8fe4b835925d0d923ac69
TWang <wang.tw.ted@gmail.com>
2026-05-12 17:58:32 +0800

    modify: install imu driver feature: detect OS and setup $DISPLAY

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p $WS_DEST
 
 if [ ! -d "$WS_DEST/ira_laser_tools" ]; then
     echo "Cloning repositories..."
-    git clone -b humble https://github.com/ros-drivers/phidgets_drivers.git $WS_DEST/phidgets_drivers
+    # git clone -b humble https://github.com/ros-drivers/phidgets_drivers.git $WS_DEST/phidgets_drivers
     git clone -b ros2 https://github.com/Slamtec/rplidar_ros.git $WS_DEST/rplidar_ros
     git clone -b humble https://github.com/nakai-omer/ira_laser_tools.git $WS_DEST/ira_laser_tools
 fi
