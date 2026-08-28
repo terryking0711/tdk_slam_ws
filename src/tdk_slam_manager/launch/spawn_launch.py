7afd7038b8197b74669fcd7ce02067f4f3e0e029
terryking0711 <terry9487turry0711@gmail.com>
2026-08-28 09:56:22 +0000

    modified lidar position( -> baselink) and lidar filtered angle

--- src/tdk_slam_manager/launch/spawn_launch.py
+++ src/tdk_slam_manager/launch/spawn_launch.py
@@ -121,8 +121,8 @@ def generate_launch_description():
         executable='laser_angle_filter_node',
         name='filter_front',
         parameters=[{
-            'lower_angle': -1.5707,
-            'upper_angle': -3.1415,
+            'lower_angle': 0.0,
+            'upper_angle': 1.5707,
             'input_topic': '/front/scan',
             'output_topic': '/front/scan_filtered'
         }]
@@ -133,8 +133,8 @@ def generate_launch_description():
         executable='laser_angle_filter_node',
         name='filter_rear',
         parameters=[{
-            'lower_angle': -1.5707,
-            'upper_angle': -3.1415,
+            'lower_angle': 0.0,
+            'upper_angle': 1.5707,
             'input_topic': '/rear/scan',
             'output_topic': '/rear/scan_filtered'
         }]
